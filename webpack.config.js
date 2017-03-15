92d51337ef1401bd3e7c4a81099bb58737ea635d
mactive <mactive@gmail.com>
2017-03-15 09:03:02 +0800

    change build dictionary

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './dist/lib'),
+    publicPath: '/dist',
     filename: 'build.js'
   },
   module: {
